2001c0e9ba65a4989d4759e2a370afa60735bb0a
Ian W <49083526+Landaman@users.noreply.github.com>
2023-04-14 10:13:40 -0400

    Fixed build issues by restoring build.gradle and updating the gradle-wrapper version

--- src/main/java/edu/wpi/teamname/HexObserver.java
+++ src/main/java/edu/wpi/teamname/HexObserver.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Label;
 public class HexObserver extends Observer {
   Subject subject;
   Label label;
+
   public HexObserver(Subject subject, Label label) {
     this.subject = subject;
     this.label = label;
--- src/main/java/edu/wpi/teamname/OctalObserver.java
+++ src/main/java/edu/wpi/teamname/OctalObserver.java
@@ -5,10 +5,12 @@ import javafx.scene.control.Label;
 public class OctalObserver extends Observer {
   Subject subject;
   Label label;
+
   public OctalObserver(Subject subject, Label label) {
     this.subject = subject;
     this.label = label;
   }
+
   @Override
   public void update() {}
 }
